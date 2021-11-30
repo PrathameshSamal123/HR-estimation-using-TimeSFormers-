098699dbddd4c1021a0813b81f932fedd885106c
Pratham <samalprathamesh123@gmail.com>
2021-11-30 14:55:42 +0530

    video to frames as required

--- dataset.py
+++ dataset.py
@@ -2,29 +2,84 @@
 
 import torch
 import torchvision
+import math
+import os 
 import torchvision.transforms as transforms
 import torchvideo.datasets as datasets
 import torchvideo.samplers as samplers
 import torchvideo.transforms as VT
 from torchvision.transforms import Compose
+import torch.optim as optim
+import cv2
 
 import numpy as np
 import pandas as pd 
 
 
 var = pd.read_csv(r"C:\Users\samal\Desktop\p1-5\p1\v1\source1\gt_HR.csv")
-
+# number of gt
 csv_len = len(var)
 
 
-x = torchvision.io.read_video(filename=r"C:\Users\samal\Desktop\p1-5\p1\v1\source1\video.avi")
-print(x[0].shape)
-total_frames = x[0].shape[0]         
-mul = int(total_frames/csv_len)       # multiplying factor
-print(mul)
+preview = []
+videos = []
 
-idx = list(range(mul,total_frames,mul))
-print(len(idx))
+DATA_DIR = r"C:\Users\samal\Desktop\hr"
 
-data = x[0][idx]
-print(data.shape)
+# min_video_frames = math.inf 
+# vidcap  = cv2.VideoCapture(r"C:\Users\samal\Desktop\hr\video.avi")
+# length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+# if length < min_video_frames:
+#      min_video_frames = length
+
+
+video_file_name = r"C:\Users\samal\Desktop\hr\video.avi"
+vidcap  = cv2.VideoCapture(video_file_name)
+print(vidcap)
+success, image = vidcap.read()
+print(success)
+print(image.shape)
+
+# getting video details (fps, amount of frames)
+fps = vidcap.get(cv2.CAP_PROP_FPS)
+amountOfFrames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT )
+print("fps:",fps)
+print("Amount of Frames:", amountOfFrames)
+
+# variables
+considered_frames_counter = 0
+FRAMES_INTERVAL = 1
+frames = []
+start_frame_number=0
+
+skip_by = int(amountOfFrames/csv_len)
+
+while success:    
+
+    if considered_frames_counter == int(amountOfFrames / FRAMES_INTERVAL) - 1:
+      break
+    
+    #### skip frames 
+    start_frame_number+=skip_by
+    vidcap.set(cv2.CAP_PROP_POS_FRAMES, start_frame_number)
+    success,image = vidcap.read()
+
+
+    if considered_frames_counter == FRAMES_INTERVAL:
+      preview.append((video_file_name, cv2.resize(image, (224,224))))
+    if success and considered_frames_counter % FRAMES_INTERVAL == 0:
+      image = np.transpose(np.asarray(cv2.resize(image, (224,224))), (2, 0, 1))
+      frames.append(image)
+    
+    if success:
+      considered_frames_counter += 1
+
+
+print("Complete")
+
+
+videos.append(frames)
+
+video = torch.tensor(np.asarray(videos)).float() # (batch x frames x channels x height x width)
+
+print(video.shape)
